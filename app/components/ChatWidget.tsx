fe11b8253f87619f25d290a5c485b45bf4cb9fb9
Gayashan Dewanarayana <87778245+gayyaalpha@users.noreply.github.com>
2026-02-13 08:09:51 +1100

    Update ChatWidget.tsx

--- app/components/ChatWidget.tsx
+++ app/components/ChatWidget.tsx
@@ -39,7 +39,7 @@ export default function ChatWidget() {
     setLoading(true);
 
     try {
-      const res = await fetch( process.env.NEXT_PUBLIC_AZURE_FUNCTION_URL! +"/api/chat_trigger", {
+      const res = await fetch( process.env.NEXT_PUBLIC_AZURE_FUNCTION_URL! , {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMessage }),
